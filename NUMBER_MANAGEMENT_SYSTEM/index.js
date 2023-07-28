8ee6d43d6e8b6afc6bb20e76b34deb768f39b5dc
01920802720 <tripathiamit2021@gmail.com>
2023-07-28 15:44:06 +0530

    handling the Query and Url

--- NUMBER_MANAGEMENT_SYSTEM/index.js
+++ NUMBER_MANAGEMENT_SYSTEM/index.js
@@ -3,3 +3,16 @@ const app  = express();
 const PORT = process.env.PORT|| 8008;
 
 app.listen(PORT , ()=>{ console.log(`Number Management service is running at port ${PORT}`)});
+
+app.get('/numbers', async (req, res) => {
+
+    //getting the url
+    const urls = req.query.url;
+
+    // url should be array
+    if (!urls || !Array.isArray(urls)) {
+      return res.status(400).json({ error: 'invalid url' });
+    
+    }
+})
+
