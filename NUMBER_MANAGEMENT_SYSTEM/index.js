b976d0ba60a1a62db622585eeb51209f9972ac93
01920802720 <tripathiamit2021@gmail.com>
2023-07-28 15:47:10 +0530

    fetching nums from each valid url

--- NUMBER_MANAGEMENT_SYSTEM/index.js
+++ NUMBER_MANAGEMENT_SYSTEM/index.js
@@ -9,6 +9,8 @@ app.get('/numbers', async (req, res) => {
     //getting the url
     const urls = req.query.url;
 
+
+
     // url should be array
     if (!urls || !Array.isArray(urls)) {
       return res.status(400).json({ error: 'invalid url' });
@@ -16,3 +18,14 @@ app.get('/numbers', async (req, res) => {
     }
 })
 
+
+async function fetchNumbers(url) {
+
+    try {
+      const response = await axios.get(url);
+      return response.data.numbers;
+} catch (error) {
+console.error(`failed to fetch data, url: ${url}`);
+return [];
+}
+  }
